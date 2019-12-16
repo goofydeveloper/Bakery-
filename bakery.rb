a10b86291a57b3b1c1cd101d808f0cbdc16a162d
Kaleab Fekade <kaleab83@gmail.com>
2019-12-15 21:16:27 -0500

    Pages are now linked,grid added. Design and api all thats left to implement

--- bakery.rb
+++ bakery.rb
@@ -1,27 +1,27 @@
-# define class: Cookies 
+# define class: Cookie 
 class Cookie
-# initialize method: name info price 
-def initialize(name,info,price)
-#  displaying values 
-@cookie_name = name 
-@cookie_info = info
-@cookie_price = price 
-# displaying values 
-puts "Name is: #@cookie_name"
-puts "Info is: #@cookie_info"
-puts "Name is: #@cookie_price"
-puts "\n"
-end 
-end
-#create objects and passing parameters to new method
-xcookie = Cookie. new("Chocolate Chip","Softbaked cookie with swiss chocolate chips that will have you feeling like your in a cozy cabin layiing of clouds","3.99")
+    # initialize method: name info price 
+    def initialize(name,info,price)
+    #  displaying values 
+    @cookie_name = name 
+    @cookie_info = info
+    @cookie_price = price 
+    # displaying values 
+    puts "Name is: #@cookie_name"
+    puts "Info is: #@cookie_info"
+    puts "Price is: #@cookie_price"
+    puts "\n"
+    end 
+    end
+    #create objects and passing parameters to new method
+    xcookie = Cookie. new("Chocolate Chip","Softbaked cookie with swiss chocolate chips that will have you feeling like your in a cozy cabin layiing of clouds","3.99")
 
-ycookie = Cookie. new("Oatmeal Cookie","Warm and healthy good after meal snack","2.99")
+    ycookie = Cookie. new("Oatmeal Cookie","Warm and healthy good after meal snack","2.99")
 
-zcookie = Cookie. new("Rasin Cookie","Tastes like freshly picked rasin with a crunchy on the outside and soft on the inside feel","2.55")
+    zcookie = Cookie. new("Rasin Cookie","Tastes like freshly picked rasin with a crunchy on the outside and soft on the inside feel","2.55")
 
 
-# define class: Cookies 
+# define class: Cake
 class Cake
     # initialize method: name info price 
     def initialize(name,info,price)
@@ -32,7 +32,7 @@ class Cake
     # displaying values 
     puts "Name is: #@cake_name"
     puts "Info is: #@cake_info"
-    puts "Name is: #@cake_price"
+    puts "Price is: #@cake_price"
     puts "\n"
     end 
     end
@@ -44,7 +44,7 @@ class Cake
     zcake = Cake. new("Vanilla Cake","Marble vanilla sponge cake with vanilla cream filling","25.55")
 
 
-    # define class: Cookies 
+# define class: Muffin 
 class Muffin
     # initialize method: name info price 
     def initialize(name,info,price)
@@ -55,7 +55,7 @@ class Muffin
     # displaying values 
     puts "Name is: #@muffin_name"
     puts "Info is: #@muffin_info"
-    puts "Name is: #@muffin_price"
+    puts "Price is: #@muffin_price"
     puts "\n"
     end 
     end
