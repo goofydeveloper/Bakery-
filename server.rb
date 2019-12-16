fd1628b36b67859e699ca47c6fb62bd5878c8cbd
Kaleab Fekade <kaleab83@gmail.com>
2019-12-15 23:04:27 -0500

    Revert "API working,css for subpages and rendering objects on page left."
    This reverts commit 0d6d034fcc8849815f763ae36dd3fb76321030da.
    
    revert changes

--- server.rb
+++ server.rb
@@ -1,6 +1,5 @@
 # myapp.rb
 require 'sinatra'
-require 'twilio-ruby'
 
 get '/' do
     erb :index
@@ -17,15 +16,3 @@ end
 get '/muffin' do
     erb :muffin
 end
-
-
-account_sid = "AC42fa2607d23158598a0267d1c8965467" # Your Test Account SID from www.twilio.com/console/settings
-auth_token = "de3c847d92ab96ae92b75cd0791e55fe"   # Your Test Auth Token from www.twilio.com/console/settings
-
-@client = Twilio::REST::Client.new account_sid, auth_token
-message = @client.messages.create(
-    body: "Thank you fo visiting Baker Joe's we loved your company, Please come again!!!",
-    to: "+19175933296",    # Replace with your phone number
-    from: "+12253195901")  # Use this Magic Number for creating SMS
-
-puts message.sid
