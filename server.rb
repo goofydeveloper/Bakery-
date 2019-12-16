14c0efb83528690efc577c43e06409594b0cc62d
Kaleab Fekade <kaleab83@gmail.com>
2019-12-15 23:10:29 -0500

    API stuff

--- server.rb
+++ server.rb
@@ -16,3 +16,14 @@ end
 get '/muffin' do
     erb :muffin
 end
+
+# account_sid = "---------------------------------" # Your Test Account SID from www.twilio.com/console/settings
+# auth_token = "------------------------------------"   # Your Test Auth Token from www.twilio.com/console/settings
+
+# @client = Twilio::REST::Client.new account_sid, auth_token
+# message = @client.messages.create(
+#     body: "Thank you fo visiting Baker Joe's we loved your company, Please come again!!!",
+#     to: "+----------------",    # Replace with your phone number
+#     from: "-------------")  # Use this Magic Number for creating SMS
+
+# puts message.sid
